ae894a7d5b79764bb6bcc90849cbc352658c7e84
Dima Cheverda <dimacheverda@Dimas-Mac-Pro.local>
2016-03-13 13:43:42 +0200

    Add custom appearance

---
+++ BlogTest/App/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  BlogTest
+//
+//  Created by Dima Cheverda on 3/13/16.
+//  Copyright © 2016 Dima Cheverda. All rights reserved.
+//
+
+import UIKit
+
+class Appearance {
+    class func setupAppearance() {
+        let barTintColor = UIColor(red:0.09, green:0.09, blue:0.11, alpha:1)
+        let tintColor = UIColor(red:1, green:0.39, blue:0, alpha:1)
+        
+        UINavigationBar.appearance().tintColor = tintColor
+        UINavigationBar.appearance().barTintColor = barTintColor
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: tintColor]
+        UINavigationBar.appearance().barStyle = .Black
+    }
+}
